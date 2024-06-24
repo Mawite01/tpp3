f1fa5a3b0ad535f51a889055397bc573ced7468d
sophia <thangneimawi@aceplussolutions.com>
2024-06-24 15:51:49 +0630

    add dashboard

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -8,22 +8,6 @@
 </head>
 <body>
     <h1>Create Product</h1>
-    <form action="{{ route('products.store')}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="">Name</label>
-        <input type="text" name="user_name" value="{{ old('name')}}"> <br> <br>
-        @error('name')
-        <span class="text-danger">*{{ $message }}</span> <br>
-        @enderror
-        <label for="">Price</label>
-        <input type="text" name="price" value="{{ old('price') }}"> <br> <br>
-        @error('price')
-        <span class="text-danger">*{{ $message }}</span> <br>
-        @enderror
-        <label for="">Image</label>
-        <input type="file" name="image"> <br> <br>
-
-        <button type="submit">Create</button>
-    </form>
+    
 </body>
 </html>
